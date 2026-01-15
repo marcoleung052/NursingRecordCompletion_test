201b3078ee53f2e73256ec8cb8e4a96dc4529911
marcoleung052 <marcoleung.cgu@gmail.com>
2026-01-15 19:42:39 +0800

    Update layout.js

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -2,7 +2,9 @@ function renderHeader(username = "未登入") {
   const header = document.createElement("header");
   header.innerHTML = `
     <div class="header-bar">
-      <div class="logo">🏥 護理紀錄系統</div>
+      <div class="logo">
+        <a href="../patients/patients.html" style="color:white;text-decoration:none;">🏥 護理紀錄系統</a>
+      </div>
       <div class="user-info">
         <span>${username}</span>
         <a href="../settings/settings.html">系統設定</a>
