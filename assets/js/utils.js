bcdd19f57fdbd9ef4e8f2f1525a656743a38b714
marcoleung052 <marcoleung.cgu@gmail.com>
2026-01-15 19:08:15 +0800

    Update utils.js

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,1 +1,9 @@
+const utils = {
+  formatDate(date) {
+    return date.toISOString().split("T")[0];
+  },
 
+  uuid() {
+    return Date.now().toString(36) + Math.random().toString(36).substr(2);
+  }
+};
